3ecd8650db7d73e450a0c9fce2df40769b5c10a5
Naufal Athallah Iwel <spealdm6@gmail.com>
2023-09-28 11:30:55 +0700

    feat: error handling route not found

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -5,8 +5,7 @@ const placesControllers = require("../controllers/places-controller");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("get req in places");
-  res.json({ message: "it worked" });
+  res.json({ message: "api mern" });
 });
 
 router.get("/:pid", placesControllers.getPlaceById);
