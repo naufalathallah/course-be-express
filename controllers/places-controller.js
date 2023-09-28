a04fc1697a7bc1004e02ff4efe187ebef1ae9f58
Naufal Athallah Iwel <spealdm6@gmail.com>
2023-09-28 13:44:11 +0700

    feat: user validation input

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -141,6 +141,9 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+  if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
+    throw new HttpError("tidak dapat menemukan tempat untuk id itu", 404);
+  }
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
   res.status(200).json({ message: "tempat terhapus" });
 };
